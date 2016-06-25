f51b07bfa41a5e8da81a0de899edc5b7ae264462
Arash Shafiei <arash.shafiei@gmail.com>
2016-06-25 11:15:49 +0800

    config

---
+++ core/factory.cpp
@@ -0,0 +1,47 @@
+/*
+ * Act Multimedia Framework (actmf)
+ * Copyright (C) 2016  Arash Shafiei
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include "factory.h"
+
+using namespace actmf;
+
+Factory::Factory()
+{
+
+}
+
+Factory::Factory(const Factory& other)
+{
+
+}
+
+Factory::~Factory()
+{
+
+}
+
+Factory& Factory::operator=(const Factory& other)
+{
+
+}
+
+bool Factory::operator==(const Factory& other) const
+{
+
+}
